65927fc956e35ff8bc854c76734b40df2b0ecc52
Marius GAUDIN <g-marius@hotmail.fr>
2022-10-21 15:30:08 +0200

    prod environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCjJ3eXR7bz5SRQDhw8XV3dp-HYoGcopns",
+    authDomain: "dashboardm2-a62bd.firebaseapp.com",
+    projectId: "dashboardm2-a62bd",
+    storageBucket: "dashboardm2-a62bd.appspot.com",
+    messagingSenderId: "599115201628",
+    appId: "1:599115201628:web:9ca70b0177cb4ffc247a99",
+    measurementId: "G-QBBNYWYZNG"
+  }
 };
